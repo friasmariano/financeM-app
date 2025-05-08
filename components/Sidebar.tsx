33b39c5cc6c8f7c5bddcf3b3f5bb196045d3bde5
friasmariano <53222843+friasmariano@users.noreply.github.com>
2025-05-08 12:42:49 -0400

    Sidebar overall styling and routes(pages) init

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,37 +1,49 @@
+'use client';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faSun } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 export default function Sidebar() {
+    const [links, setLinks] = useState([
+        { href: '/', label: 'Overview', icon: 'bi-house', hoverIcon: 'bi-house-fill', hovered: false },
+        { href: '/transactions', label: 'Transactions', icon: 'bi bi-clipboard-data', hoverIcon: 'bi bi-clipboard-data-fill', hovered: false },
+        { href: '/budgets', label: 'Budgets', icon: 'bi-pie-chart', hoverIcon: 'bi bi-pie-chart-fill', hovered: false },
+        { href: '/bills', label: 'Bills', icon: 'bi-file-earmark-break', hoverIcon: 'bi-file-earmark-break-fill', hovered: false },
+    ])
+
+    const pathname = usePathname();
+
+    const setHover = (index: number, isHovering: boolean) => {
+        setLinks((prev) =>
+            prev.map((link, i) =>
+                i === index ? { ...link, hovered: isHovering } : link
+            )
+        )
+    }
+
     return (
         <section className="sidebar">
-            <div style={{ display: 'flex', padding: '30px 0px 30px 20px' }}>
+            <div style={{ display: 'flex', padding: '50px 0px 40px 40px' }}>
                 <h1 style={{ fontSize: '1.3rem', fontWeight: '600'}}>
                     financeM
                 </h1>
             </div>
 
             <ul>
-                <li style={{ padding: '15px 0px 20px 20px' }}>
-                    <i className="bi bi-house"
-                       style={{ marginRight: '10px' }}></i>
-                    Overview
+               {links.map((link, index) => (
+                <li key={link.href}
+                    style={{ padding: '10px 0px 10px 40px', margin: '0px 0px 12px 0px'}}
+                    onMouseEnter={() => setHover(index, true)}
+                    onMouseLeave={() => setHover(index, false)}
+                    className={pathname === link.href ? 'sidebar-active-route' : ''}
+                    >
+                    <Link href={link.href}>
+                        <i className={`bi ${link.hovered || pathname === link.href ? link.hoverIcon : link.icon} mr-2`}></i>
+                        <span>{link.label}</span>
+                    </Link>
                 </li>
-                <li style={{ padding: '0px 0px 20px 20px' }}>
-                    <i className="bi bi-arrow-left-right"
-                       style={{ marginRight: '10px' }}></i>
-                    Transactions
-                </li>
-                <li style={{ padding: '0px 0px 20px 20px' }}>
-                    <i className="bi bi-pie-chart"
-                       style={{ marginRight: '10px' }}></i>
-                    Budgets
-                </li>
-                <li style={{ padding: '0px 0px 20px 20px' }}>
-                    <i className="bi bi-file-earmark-break"
-                       style={{ marginRight: '10px' }}></i>
-                    Bills
-                </li>
+               ))}
             </ul>
         </section>
     )
