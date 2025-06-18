6c7237614dac4012b5ade2f61dfc4a30e29c89fc
friasmariano <53222843+friasmariano@users.noreply.github.com>
2025-06-18 11:59:22 -0400

    Middleware upd

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,16 @@ import type { NextRequest } from "next/server";
 import { verify } from "jsonwebtoken";
 
 const PUBLIC_PATHS = ['/', '/login'];
+const PUBLIC_ONLY_PATHS = ['/', '/login'];
 
 export function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
     const token = request.cookies.get("jwt")?.value;
 
     const isPublic = PUBLIC_PATHS.some(path => request.nextUrl.pathname.startsWith(path));
+    const isPublicOnly = PUBLIC_ONLY_PATHS.includes(pathname);
 
-    if (isPublic)
+    if (isPublic && !token)
         return NextResponse.next();
 
     if (!token)
@@ -17,6 +20,11 @@ export function middleware(request: NextRequest) {
 
     try {
         verify(token, process.env.JWT_PUBLIC_KEY!);
+
+        if (isPublicOnly) {
+            return NextResponse.redirect(new URL('/dashboard', request.url));
+        }
+
         return NextResponse.next();
     } catch (error) {
         if (process.env.NODE_ENV !== 'production') {
@@ -25,6 +33,7 @@ export function middleware(request: NextRequest) {
 
         return NextResponse.redirect(new URL('/login', request.url));
     }
+
 }
 
 export const config = {
@@ -32,6 +41,8 @@ export const config = {
         '/dashboard/:path*',
         '/bills/:path*',
         '/budgets/:path*',
-        '/transactions/:path*'
+        '/transactions/:path*',
+        '/login',
+        '/'
     ]
 }
